b1e3f21b8ebf22aaeaaace5cd76b0cbbdd552bbd
Paia <paiambusiness@gmail.com>
2024-01-23 14:58:35 +1300

    semi-working & removal of secrets

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const request = require('request');
 const dotenv = require('dotenv');
 
 const port = 5000
@@ -8,7 +9,6 @@ dotenv.config()
 var spotify_client_id = process.env.SPOTIFY_CLIENT_ID
 var spotify_client_secret = process.env.SPOTIFY_CLIENT_SECRET
 
-var app = express();
 
 var generateRandomString = function (length) {
   var text = '';
@@ -20,22 +20,11 @@ var generateRandomString = function (length) {
   return text;
 };
 
+var app = express();
+
 app.get('/auth/login', (req, res) => {
-});
 
-app.get('/auth/callback', (req, res) => {
-});
-
-app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`)
-})
-
-router.get('/auth/login', (req, res) => {
-
-  var scope = "streaming \
-               user-read-email \
-               user-read-private"
-
+  var scope = "streaming user-read-email user-read-private"
   var state = generateRandomString(16);
 
   var auth_query_parameters = new URLSearchParams({
@@ -44,7 +33,46 @@ router.get('/auth/login', (req, res) => {
     scope: scope,
     redirect_uri: "http://localhost:3000/auth/callback",
     state: state
-  })
+  });
+
+app.get('/auth/callback', (req, res) => {
+
+  var code = req.query.code;
+
+  var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    form: {
+      code: code,
+      redirect_uri: "http://localhost:3000/auth/callback",
+      grant_type: 'authorization_code'
+    },
+    headers: {
+      'Authorization': 'Basic ' + (Buffer.from(spotify_client_id + ':' + spotify_client_secret).toString('base64')),
+      'Content-Type' : 'application/x-www-form-urlencoded'
+    },
+    json: true
+  };
+
+  request.post(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var access_token = body.access_token;
+      res.redirect('/')
+    }
+  });
+})
+
+
 
   res.redirect('https://accounts.spotify.com/authorize/?' + auth_query_parameters.toString());
+});
+
+app.get('/auth/token', (req, res) => {
+  res.json(
+     {
+        access_token: access_token
+     })
+});
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`)
 })
